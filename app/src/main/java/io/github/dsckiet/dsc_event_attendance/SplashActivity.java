5b4b5e3daca9cf8da9f490c4404f4ef46561c0ad
Aakash Goel <goelaakash79@gmail.com>
2019-03-03 13:48:00 +0530

    Initial commit

---
+++ app/src/main/java/io/github/dsckiet/dsc_event_attendance/SplashActivity.java
@@ -0,0 +1,65 @@
+package io.github.dsckiet.dsc_event_attendance;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        ImageView logolayout = findViewById(R.id.logo);
+        logolayout.animate().alpha(1.0f).scaleX(0.9f).scaleY(0.9f).setDuration(2500);
+
+        Boolean isFirstRun = getSharedPreferences("PREFERENCES", MODE_PRIVATE)
+                .getBoolean("isfirstrun", true);
+
+        if (isFirstRun) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(getApplicationContext(), AuthActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, 3000);
+
+            getSharedPreferences("PREFERENCES", MODE_PRIVATE).edit()
+                    .putBoolean("isfirstrun", false).commit();
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    if (user == null) {
+                        Intent i = new Intent(getApplicationContext(), AuthActivity.class);
+                        startActivity(i);
+                        finish();
+                        finishActivity(0);
+                    } else {
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                        finish();
+                        finishActivity(0);
+                    }
+                }
+            }, 2600);
+        }
+
+    }
+}
